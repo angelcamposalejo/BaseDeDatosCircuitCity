cd895146fdd4889156e375623316efda5537228b
angelmarquesadacelular <angelcamposalejo@gmail.com>
2017-05-29 13:12:51 -0500

    insercion de datos base de datos circuit city

--- circuit city base de datos.sql
+++ circuit city base de datos.sql
@@ -30,3 +30,40 @@ CREATE TABLE ordex (
     FOREIGN KEY (client_id)
         REFERENCES client (client_id)
 );
+insert into item(name,cost)values
+('Monitor X8','200.50'),
+('Monitor X2','399.50'),
+('Monitor S3','454.50'),
+('Hard Drive 15G','80.99'),
+('Hard Drive 80G','280.00'),
+('Printer U9','550.50'),
+('CRDOM','39.50'),
+('Cd Burne','99.78'),
+('RAM 255M','54.50'),
+('RAM 512M','80.99'),
+('Keyboard','20.00'),
+('Mouse','15.68'),
+('USB drive','15.68');
+insert into ord_det(item_id,item_count)values
+(1,4),(5,5),(6,1),(7,3),(10,2),(3,5),(4,2),(11,4),(12,12),
+(8,5),(4,2),(5,2),(3,2),(13,10);
+insert into client(name,company)values
+('Smith, Tony','Microsoft'),
+('Smith, Julie','IBM'),
+('Fry, Kevin','IBM'),
+('Flana, Fran','Microsoft'),
+('Belleza, Ana','Canon'),
+('DiTore, Mike','Microsoft'),
+('Ferry, Jim','HP'),
+('Lynch, Tim','DELL'),
+('Frunch, Ken','Pacific Bell');
+insert into credit_client(client_id,credit)values
+(1,'1000.00'),
+(2,'3000.00'),
+(3,'400.00'),
+(4,'500.00'),
+(5,'7000.00'),
+(6,'8000.00'),
+(9,'90.00');
+insert into ordex(client_id)values
+(1),(9),(6),(2),(9),(2);
